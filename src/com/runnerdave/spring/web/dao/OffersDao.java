90e2c4da7c64881928fcadd722fbc6b86ab8fe21
runnerdave <runnerdave@gmail.com>
2015-12-13 12:32:54 +1100

    complete removal of jdbc template

--- src/com/runnerdave/spring/web/dao/OffersDao.java
+++ src/com/runnerdave/spring/web/dao/OffersDao.java
@@ -2,15 +2,9 @@ package com.runnerdave.spring.web.dao;
 
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,15 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Component("offersDao")
 public class OffersDao extends BaseDao {
 
-	private NamedParameterJdbcTemplate jdbc;
-
 	public OffersDao() {
 		System.out.println("Succesfully loaded Offers DAO");
-	}
-
-	@Autowired
-	public void setDataSource(DataSource jdbc) {
-		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -56,17 +43,17 @@ public class OffersDao extends BaseDao {
 	}
 
 	@Transactional
-	public int[] create(List<Offer> offers) {
+	public void create(List<Offer> offers) {
 
-		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
-
-		return jdbc.batchUpdate("insert into offers (text, username) values (:text, :username)", params);
+		for (Offer offer : offers) {
+			this.saveOrUpdate(offer);
+		}
 	}
 
 	public boolean delete(int id) {
-		
+
 		Query query = session().createQuery("delete from Offer where id=:id");
-		
+
 		query.setLong("id", id);
 		return query.executeUpdate() == 1;
 	}
